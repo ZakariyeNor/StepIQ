886cb5fe5bdf36bbd048548431989487744b6a17
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-06-26 11:17:13 +0000

    Added responsive style for the  header layout for different screen sizes

--- static/js/base.js
+++ static/js/base.js
@@ -1,8 +1,2 @@
 /*  Toggle Script */
-  document.addEventListener('DOMContentLoaded', function () {
-    const burger = document.getElementById('burgerMenu');
-    const mobileNav = document.getElementById('mobileNav');
-    burger.addEventListener('click', function () {
-      mobileNav.classList.toggle('d-none');
-    });
-  });
+  
