9d3e600733c02f220a00b33a54bfb46a82b8c627
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-06-26 15:12:27 +0000

    Created mobile sidebar navigation, made it responsive and gave the style

--- static/js/base.js
+++ static/js/base.js
@@ -1,2 +1,12 @@
 /*  Toggle Script */
-  
+document.addEventListener('DOMContentLoaded', function() {
+    /* Mobile menu toggle */
+    const burgerIcon = document.querySelector('.menu-icon');
+    const listItems = document.querySelector('.mobile-nav');
+
+    if(burgerIcon && listItems) {
+        burgerIcon.addEventListener('click', function() {
+            listItems.classList.toggle('open')
+        });
+    }
+});
