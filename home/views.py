72203a6d6c6d110c0f96be6dfd7a2a05dca006b8
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-06-27 10:23:29 +0000

    Added the hero section image, marketing paragraph and action button shop now

--- home/views.py
+++ home/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
 def index(request):
     """ A view to render home page"""
+    hero_image = settings.MEDIA_URL + 'hero_pict.jpg'
     return render(request, "home/index.html", {
         "company_name": "Lanezra",
+        "hero_image": hero_image,
     })
