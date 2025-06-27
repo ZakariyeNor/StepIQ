b7668b6151e311516bc0b6526554ebff71f86123
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-06-27 15:39:10 +0000

    Added search input form and it's functionality

--- static/js/base.js
+++ static/js/base.js
@@ -4,9 +4,22 @@ document.addEventListener('DOMContentLoaded', function() {
     const burgerIcon = document.querySelector('.menu-icon');
     const listItems = document.querySelector('.mobile-nav');
 
+    const searchToggle = document.getElementById('searchToggle');
+    const searchForm = document.getElementById('searchFormContainer');
+
     if(burgerIcon && listItems) {
         burgerIcon.addEventListener('click', function() {
             listItems.classList.toggle('open')
         });
     }
+
+    searchToggle.addEventListener('click', function(e) {
+        e.preventDefault();
+        searchForm.classList.toggle('d-none');
+        if (!searchForm.classList.contains('d-none')) {
+        // Focus input when shown
+        const input = searchForm.querySelector('input[name="q"]');
+        if (input) input.focus();
+      }
+    });
 });
