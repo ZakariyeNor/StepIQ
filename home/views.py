1d54b3bcf09c0f8ca62c0b6efe0ca90051226cf2
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-06-27 14:07:39 +0000

    Added categories section

--- home/views.py
+++ home/views.py
@@ -5,7 +5,15 @@ from django.conf import settings
 def index(request):
     """ A view to render home page"""
     hero_image = settings.MEDIA_URL + 'hero_pict.jpg'
+    sports = settings.MEDIA_URL + 'sports.jpg'
+    running = settings.MEDIA_URL + 'running.jpg'
+    casual = settings.MEDIA_URL + 'casual.jpg'
+    formal = settings.MEDIA_URL + 'formal.jpg'
     return render(request, "home/index.html", {
         "company_name": "Lanezra",
         "hero_image": hero_image,
+        "running": running,
+        "casual": casual,
+        "formal": formal,
+        "sports": sports,
     })
